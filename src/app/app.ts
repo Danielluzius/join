cadee469c373014cb74e77b97089870bb67fe8b5
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-10-20 14:01:10 +0200

    console log for contacts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('join');
+export class App implements OnInit {
+  private firestore = inject(Firestore);
+
+  ngOnInit() {
+    const contactsRef = collection(this.firestore, 'contacts');
+    onSnapshot(contactsRef, snapshot => {
+      snapshot.forEach(doc => console.log(doc.id, doc.data()));
+    });
+  }
 }
