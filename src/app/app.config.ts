6d129c9896bbb301c77bc342c109b68cf031af89
PatrickSchmidtDev <p.schmidt.391994@gmail.com>
2025-10-20 13:27:42 +0200

    Connect Database

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "join-554eb",
+      "appId": "1:949596014477:web:7c0328626b1faaf4fadfc4",
+      "storageBucket": "join-554eb.appspot.com",
+      "apiKey": "AIzaSyDFn8HmBoR5x0S2UnGjaGzxfoxy_Pd5BoA",
+      "authDomain": "join-554eb.firebaseapp.com",
+      "messagingSenderId": "949596014477",
+      "measurementId": "G-1DYD7G0MD3"
+    })),
+    provideFirestore(() => getFirestore())
   ]
 };
