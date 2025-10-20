63a07a21e279d945fec9f1fb01b7dc61e30be081
Daniel <Danielluzius@mail.de>
2025-10-20 12:38:36 +0200

    feat(shared): add header and navbar components feat(features): add initial feature components for navbar content

--- src/app/features/summary/summary.spec.ts
+++ src/app/features/summary/summary.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Footer } from './footer';
+import { Summary } from './summary';
 
-describe('Footer', () => {
-  let component: Footer;
-  let fixture: ComponentFixture<Footer>;
+describe('Summary', () => {
+  let component: Summary;
+  let fixture: ComponentFixture<Summary>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Footer]
+      imports: [Summary]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Footer);
+    fixture = TestBed.createComponent(Summary);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
