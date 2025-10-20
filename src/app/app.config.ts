b75b895deebe75bfcc7f55403d684b7205c30a94
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-10-20 13:54:59 +0200

    Database connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,15 +8,14 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp({
-      "projectId": "join-554eb",
-      "appId": "1:949596014477:web:7c0328626b1faaf4fadfc4",
-      "storageBucket": "join-554eb.appspot.com",
-      "apiKey": "AIzaSyDFn8HmBoR5x0S2UnGjaGzxfoxy_Pd5BoA",
-      "authDomain": "join-554eb.firebaseapp.com",
-      "messagingSenderId": "949596014477",
-      "measurementId": "G-1DYD7G0MD3"
-    })),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ 
+      projectId: "join-554eb", 
+      appId: "1:949596014477:web:7c0328626b1faaf4fadfc4", 
+      storageBucket: "join-554eb.firebasestorage.app", 
+      apiKey: "AIzaSyDFn8HmBoR5x0S2UnGjaGzxfoxy_Pd5BoA", 
+      authDomain: "join-554eb.firebaseapp.com", 
+      messagingSenderId: "949596014477", 
+      measurementId: "G-1DYD7G0MD3", 
+    })), provideFirestore(() => getFirestore())
   ]
 };
